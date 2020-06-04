627d44af1751071f3b39f1d717938c5978bebded
Martin <blackit@arcor.de>
2020-06-04 03:45:52 +0200

    added new network and changed raiden docker to master

--- household_network/start.sh
+++ household_network/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 gnome-terminal --title="network1" --tab -- /bin/bash -c \
-'cd network0; sudo docker-compose up; read' &
+'cd network0; sudo docker-compose up --build; read' &
 
 gnome-terminal --title="network2" --tab -- /bin/bash -c \
-'cd network1; sudo docker-compose up; read'
+'cd network1; sudo docker-compose up --build; read'
